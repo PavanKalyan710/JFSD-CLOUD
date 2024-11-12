93b3e25751050dcbdba5c626ede69ae5f2200549
AKINA PAVAN KALYAN <159229291+PavanKalyan710@users.noreply.github.com>
2024-11-12 14:42:39 +0530

    Add files via upload

---
+++ sc-s13-client/src/main/java/com/example/demo/ScS13ClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScS13ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScS13ClientApplication.class, args);
+	}
+
+}
